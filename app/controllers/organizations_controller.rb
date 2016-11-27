599403d9cef948e85e6e643bcf2806c73e12fb46
Xunze Su <xunzesu@Xunzes-MacBook-Pro.local>
2016-11-27 13:57:23 -0500

    Added average rating score and the most recent comment on organization show page and controller action.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -5,5 +5,21 @@ class OrganizationsController < ApplicationController
 
   def show
     @organization = Organization.find(params[:id])
+    @ratings = @organization.ratings
+
+    # The average rating of this organization, rounded to the nearest 10th
+    @average = @ratings.pluck(:score).empty? ? 'No ratings yet. Be the first to rate it!' : @ratings.average(:score).round(1)
+
+    # The most recent non-empty comment on this organization
+    if @ratings.pluck(:comment).empty?
+      @comment = 'No comments yet. Be the first to leave a comment!'
+    else
+      @comments = @ratings.pluck(:comment).reverse
+      @comments.select! do |element|
+        element != ''
+      end
+      @comment = @comments.first
+    end
+    
   end
 end
