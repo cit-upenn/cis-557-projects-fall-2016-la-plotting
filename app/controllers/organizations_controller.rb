ffaf59e9030af42b247f9a5b04368ef76d5447bc
Xunze Su <xunzesu@Xunzes-MacBook-Pro.local>
2016-12-16 19:58:33 -0500

    Put the yelp token to environment variable. gitignore the secret.yml

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -12,8 +12,9 @@ class OrganizationsController < ApplicationController
     @average = avg_ratings_cal(@ratings)
     # The most recent non-empty comment on this organization
     @comment = recent_comment(@ratings)
-    # Three restaurants in 500 meters radius of this organization from yelp_token
+    # Three restaurants in 1500 meters radius of this organization from yelp_token
     @restaurants = find_yelp_business(@organization[:latitude], @organization[:longitude])
+    #the paloma gem uses this to pass variables from contoller to javascript
     js :organization => @organization, :restaurants => @restaurants
   end# end of a method
 
@@ -37,8 +38,8 @@ class OrganizationsController < ApplicationController
     end# end of method
 
     def find_yelp_business(lat, lng)
-      yelp_token = 'Bd-zPkv8p5CQldgybJ9mtbEt-5ll_frcOgPCgKTY9j_81fG9-5mdq6C9QIXJQIRguCE8NOP17oWDCB021qrvos_jElK-ebJrP8YOcmCzpkxW4nViDZgCSoEzDhc7WHYx'
-      header = "Bearer " + yelp_token
+      # the yelp token is set to an environment variable
+      header = "Bearer " + ENV['YELP_TOKEN']
       url = 'https://api.yelp.com/v3/businesses/search?latitude=' + lat.to_s + '&longitude=' + lng.to_s + '&radius=1500&limit=3&categories=restaurants'
       open(url, "Authorization" => header) do |f|
         query_result_string = f.read
