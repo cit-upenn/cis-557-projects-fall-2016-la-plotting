49673380f49eedafe6e5ca7388b171241f01415e
Xunze Su <xunzesu@Xunzes-MacBook-Pro.local>
2017-02-19 15:24:27 -0500

    seed new dataset

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,16 +31,38 @@ require 'csv'
 # puts "There are now #{Organization.count} rows in the organization table"
 
 # Destroyed the sample dataset
-old_ids = [1655, 1646, 1711, 1841, 9241, 2727, 1853, 2023, 1703]
+# old_ids = [1655, 1646, 1711, 1841, 9241, 2727, 1853, 2023, 1703]
+#
+# old_ids.each do |old_id|
+#   org = Organization.find_by(id: old_id)
+#   org.destroy
+# end
+#
+# # Destroy the empty organziations
+# empty_org_ids = [9242, 9243]
+# empty_org_ids.each do |empty_org_id|
+#   org = Organization.find_by(id: empty_org_id)
+#   org.destroy
+# end
 
-old_ids.each do |old_id|
-  org = Organization.find_by(id: old_id)
-  org.destroy
+# Seed the actual db of all organizations in LA
+csv_text = File.read(Rails.root.join("lib", "seeds", "LA-Data.csv"))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Organization.new
+  t.id = row['OrgID']
+  t.name = row['Name']
+  t.size = row['Size']
+  t.org_type = row['Type']
+  t.address = row['StAdd']
+  t.city = row['City']
+  t.zip = row['Zip']
+  t.county = row['County']
+  t.state = row['State']
+  t.latitude = row['Latitude']
+  t.longitude = row['Longitude']
+  t.save
+  puts "#{t.id}, #{t.name} saved"
 end
 
-# Destroy the empty organziations
-empty_org_ids = [9242, 9243]
-empty_org_ids.each do |empty_org_id|
-  org = Organization.find_by(id: empty_org_id)
-  org.destroy
-end
+puts "There are now #{Organization.count} rows in the organization table"
